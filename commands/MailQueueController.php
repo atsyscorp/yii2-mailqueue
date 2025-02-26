8afce1fbedef7653e4dd6c35cea8b1ba46bb6659
atsyscorp <69642420+atsyscorp@users.noreply.github.com>
2025-02-26 18:48:43 -0500

    Update MailQueueController.php

--- commands/MailQueueController.php
+++ commands/MailQueueController.php
@@ -2,19 +2,14 @@
 
 /**
  * Mail Command Controller
- * 
- * @author Rochdi B. <rochdi80tn@gmail.com>
  */
 
-namespace nterms\mailqueue\commands;
+namespace atsys\mailqueue\commands;
 
 use yii\console\Controller;
 
 /**
  * This command processes the mail queue
- *
- * @author Rochdi B. <rochdi80tn@gmail.com>
- * @since 0.0.6
  */
 class MailQueueController extends Controller
 {

1e65038180a3ebc1f3d981673c2c9c9fc6b881df
ATSYS <soporte@atsys.co>
2025-02-28 20:42:01 -0500

    Fixed Message.php overwriting serialize and unserialize for data.

--- Message.php
+++ Message.php
@@ -10,13 +10,33 @@ use atsyscorp\mailqueue\models\Queue;
  *
  * @see https://www.yiiframework.com/extension/yiisoft/yii2-symfonymailer
  */
-class Message extends \yii\symfonymailer\Message
+class Message extends \yii\symfonymailer\Message implements \Serializable
 {
-
-    public function __sleep()
+    public function serialize()
     {
-        // Devuelve solo las propiedades que existen y deseas serializar
-        return ['from', 'to', 'cc', 'bcc', 'subject', 'textBody', 'htmlBody'];
+        // Serializa manualmente las propiedades que necesitas
+        return serialize([
+            'from' => $this->getFrom(),
+            'to' => $this->getTo(),
+            'cc' => $this->getCc(),
+            'bcc' => $this->getBcc(),
+            'subject' => $this->getSubject(),
+            'textBody' => $this->getTextBody(),
+            'htmlBody' => $this->getHtmlBody(),
+        ]);
+    }
+
+    public function unserialize($data)
+    {
+        // Deserializa manualmente las propiedades
+        $data = unserialize($data);
+        $this->setFrom($data['from']);
+        $this->setTo($data['to']);
+        $this->setCc($data['cc']);
+        $this->setBcc($data['bcc']);
+        $this->setSubject($data['subject']);
+        $this->setTextBody($data['textBody']);
+        $this->setHtmlBody($data['htmlBody']);
     }
 
     /**
@@ -32,19 +52,24 @@ class Message extends \yii\symfonymailer\Message
             $time_to_send = time();
         }
 
-        $from = $this->getFrom() ? array_keys($this->getFrom())[0] : null;
-        $to = $this->getTo() ? array_keys($this->getTo())[0] : null;
-        $cc = $this->getCc() ? array_keys($this->getCc())[0] : null;
-        $bcc = $this->getBcc() ? array_keys($this->getBcc())[0] : null;
-        $replyTo = $this->getReplyTo() ? array_keys($this->getReplyTo())[0] : null;
+        // Obtener las direcciones de correo
+        $from = $this->getFrom() ? $this->getFrom()[0]->getAddress() : null;
+        $to = $this->getTo() ? $this->getTo()[0]->getAddress() : null;
+        $cc = $this->getCc() ? $this->getCc()[0]->getAddress() : null;
+        $bcc = $this->getBcc() ? $this->getBcc()[0]->getAddress() : null;
+        $replyTo = $this->getReplyTo() ? $this->getReplyTo()[0]->getAddress() : null;
+
+        // Obtener el asunto y los cuerpos del correo
         $subject = $this->getSubject();
         $textBody = method_exists($this, 'getTextBody') ? $this->getTextBody() : null;
         $htmlBody = method_exists($this, 'getHtmlBody') ? $this->getHtmlBody() : null;
 
+        // Validar campos obligatorios
         if (empty($from) || empty($to)) {
             throw new \yii\base\InvalidConfigException('Fields "from" and "to" are required.');
         }
 
+        // Crear un nuevo registro en la cola de correos
         $item = new Queue();
         $item->from = $from;
         $item->to = $to;
@@ -55,13 +80,14 @@ class Message extends \yii\symfonymailer\Message
         $item->text_body = $textBody;
         $item->html_body = $htmlBody;
         $item->charset = 'UTF-8';
-        $item->created_at = time();
+        $item->created_at = date('Y-m-d H:i:s');
         $item->attempts = 0;
         $item->time_to_send = date('Y-m-d H:i:s', $time_to_send);
         $item->mailer_message = base64_encode(serialize($this));
 
-        if(!$item->save()) {
-            throw new \yii\base\InvalidConfigException( json_encode($item->getErrors()) );
+        // Guardar el registro
+        if (!$item->save()) {
+            throw new \yii\base\InvalidConfigException(json_encode($item->getErrors()));
         } else {
             return true;
         }
